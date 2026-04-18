c06783516b0cb9519e1dd0ccb6b3366cdc8f6691
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-04-18 16:47:06 +0300

    feat(tests): add umbrella header

---
+++ include/vix/tests/tests.hpp
@@ -0,0 +1,29 @@
+/**
+ *
+ *  @file tests.hpp
+ *  @author Gaspard Kirira
+ *
+ *  Copyright 2025, Gaspard Kirira.
+ *  All rights reserved.
+ *  https://github.com/vixcpp/vix
+ *
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Vix.cpp
+ */
+
+#ifndef VIX_TESTS_HPP
+#define VIX_TESTS_HPP
+
+#include <vix/tests/Version.hpp>
+#include <vix/tests/Assert.hpp>
+#include <vix/tests/TestCase.hpp>
+#include <vix/tests/TestSuite.hpp>
+#include <vix/tests/TestRegistry.hpp>
+#include <vix/tests/TestRunner.hpp>
+#include <vix/tests/Summary.hpp>
+#include <vix/tests/Timer.hpp>
+#include <vix/tests/Colors.hpp>
+
+#endif // VIX_TESTS_HPP
